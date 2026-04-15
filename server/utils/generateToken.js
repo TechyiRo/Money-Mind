493ddad4ec82cc620656790f4c90eebbb6b4bf7b
TechyiRo <rsrohya@gmail.com>
2026-04-15 15:53:43 +0530

    Fix cross-domain cookie issue for deployment

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -7,8 +7,8 @@ const generateToken = (res, userId) => {
 
   res.cookie('jwt', token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV !== 'development',
-    sameSite: 'strict',
+    secure: true, // Must be true for sameSite 'none'
+    sameSite: 'none', // Required for cross-domain cookies (Vercel -> Render)
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
   });
 };
