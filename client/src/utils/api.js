547c8b529e13b58b7f889f219ce1d18eb5e2ad41
TechyiRo <rsrohya@gmail.com>
2026-04-15 16:32:38 +0530

    Implement Vercel reverse-proxy for 1st-party cookies

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,11 +1,15 @@
 import axios from 'axios';
 
 let backendUrl = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
-if (backendUrl.endsWith('/')) {
-  backendUrl = backendUrl.slice(0, -1);
-}
-if (!backendUrl.endsWith('/api')) {
-  backendUrl += '/api';
+if (import.meta.env.PROD) {
+  backendUrl = '/api'; 
+} else {
+  if (backendUrl.endsWith('/')) {
+    backendUrl = backendUrl.slice(0, -1);
+  }
+  if (!backendUrl.endsWith('/api')) {
+    backendUrl += '/api';
+  }
 }
 
 const api = axios.create({
